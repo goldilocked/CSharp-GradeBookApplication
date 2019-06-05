49eafe6685856571e01dae6de8eaf60e0fedd426
Sarah Kenny <Sarah.Kenny@huskers.unl.edu>
2019-06-05 18:38:46 -0500

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,44 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System;
+using System.Linq;
+
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            
+            if(grades[threshold - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if(grades[(2*threshold) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if(grades[(3*threshold) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if(grades[(4*threshold) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+
+            return 'F';
+
         }
     }
 }
